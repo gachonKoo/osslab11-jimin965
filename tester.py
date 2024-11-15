a700c4d6a480d2d987a0fdfd81ce23f9306e3089
jjm10 <jjm101305@gachon.ac.kr>
2024-11-15 15:02:32 +0900

    4th

--- tester.py
+++ tester.py
@@ -1,10 +1,10 @@
 import geo.utils as utils
 
 a, b=3, 4
-c = utils.pythagoras(a, b)
+c = utils.pythagoras(3, 4)
 print('c=', c)
 
 r = 10
-area = utils.circle(r)
+area = utils.circle(10)
 print('area=', area)
 
